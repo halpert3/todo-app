9cc7046fbd51d79834d763c40fbe904beb14c3a0
Henry Alpert <halpert3@gmail.com>
2017-12-06 09:38:26 -0600

    Add ability to create tasks

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,4 +1,5 @@
 class UserTasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
 
   # GET /user_tasks
@@ -29,9 +30,11 @@ class UserTasksController < ApplicationController
     respond_to do |format|
       if @user_task.save
         format.html { redirect_to @user_task, notice: 'User task was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @user_task }
       else
         format.html { render :new }
+        format.js { render :new }
         format.json { render json: @user_task.errors, status: :unprocessable_entity }
       end
     end
@@ -70,5 +73,9 @@ class UserTasksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_task_params
       params.require(:user_task).permit(:description, :due)
+    end
+
+    def all_tasks
+        @user_tasks = UserTask.all
     end
 end
