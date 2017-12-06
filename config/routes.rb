278fd69793eaee3db5df0d50603aff90c6003c10
Henry Alpert <halpert3@gmail.com>
2017-12-06 09:18:01 -0600

    Render form with javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :user_tasks
+
+  root 'user_tasks#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
