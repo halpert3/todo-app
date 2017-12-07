57753de744b57258dc6e2c1defc0884073639b60
Henry Alpert <halpert3@gmail.com>
2017-12-07 09:25:49 -0600

    Set up user model with omniauth gem

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.name = auth['info']['name']
+    end
+  end
+  # This info comes from the gem documentation for omniauth facebook we installed
+
+  def small_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=small"
+  end
+
+  def normal_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=normal"
+  end
+
+end
